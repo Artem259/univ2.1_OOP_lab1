6267bfa074888af92b054a5bea4502848dfb462a
Artem Poliakov <artem4250@gmail.com>
2021-09-30 02:16:01 +0300

    update

--- Geometry.h
+++ Geometry.h
@@ -1,6 +1,48 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
+struct Point;
+class Line;
+class Circle;
 
+//---------------------------------------------------------------------------------------------------------------//
+
+struct Point
+{
+    double x;
+    double y;
+};
+
+class Line
+{
+private:
+
+public:
+
+};
+
+class Circle
+{
+private:
+    Point center;
+    double radius;
+public:
+    Circle(Point center, double radius);
+    explicit Circle(double radius);
+};
+
+//---------------------------------------------------------------------------------------------------------------//
+
+Circle::Circle(Point center, double radius)
+{
+    this->center = center;
+    this->radius = radius;
+}
+
+Circle::Circle(double radius)
+{
+    this->center = {0,0};
+    this->radius = radius;
+}
 
 #endif
