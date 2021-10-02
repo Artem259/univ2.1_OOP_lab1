b31d293b4c14f47cf0abe9ab1ebd9d97f0904b61
Artem Poliakov <artem4250@gmail.com>
2021-10-02 03:01:16 +0300

    [Geometry.h: getters, setters]

--- Geometry.h
+++ Geometry.h
@@ -13,36 +13,109 @@ struct Point
     double y;
 };
 
-class Line
+class Line //format: ax+by+c=0
 {
 private:
-
+    double a,b,c;
 public:
-
+    Line(double _a, double _b, double _c);
+    void set(double _a, double _b, double _c);
+    void setA(double _a);
+    void setB(double _b);
+    void setC(double _c);
+    double getA() const;
+    double getB() const;
+    double getC() const;
 };
 
 class Circle
 {
 private:
-    Point center;
+    Point center{};
     double radius;
 public:
-    Circle(Point center, double radius);
-    explicit Circle(double radius);
+    Circle(Point _center, double _radius);
+    explicit Circle(double _radius);
+    void setCenter(Point _center);
+    void serRadius(double _radius);
+    Point getCenter() const;
+    double getRadius() const;
 };
 
 //---------------------------------------------------------------------------------------------------------------//
 
-Circle::Circle(Point center, double radius)
+Line::Line(double _a, double _b, double _c)
 {
-    this->center = center;
-    this->radius = radius;
+    assert(!(_a==0 && _b==0));
+    a = _a;
+    b = _b;
+    c = _c;
 }
 
-Circle::Circle(double radius)
+void Line::set(double _a, double _b, double _c)
 {
-    this->center = {0,0};
-    this->radius = radius;
+    assert(!(_a==0 && _b==0));
+    a = _a;
+    b = _b;
+    c = _c;
+}
+void Line::setA(double _a)
+{
+    a = _a;
+}
+void Line::setB(double _b)
+{
+    b = _b;
+}
+void Line::setC(double _c)
+{
+    c = _c;
+}
+
+double Line::getA() const
+{
+    return a;
+}
+double Line::getB() const
+{
+    return b;
+}
+double Line::getC() const
+{
+    return c;
+}
+
+//---------------------------------------------------------------------------------------------------------------//
+
+Circle::Circle(Point _center, double _radius)
+{
+    assert(_radius!=0);
+    center = _center;
+    radius = _radius;
+}
+Circle::Circle(double _radius)
+{
+    assert(_radius!=0);
+    center = {0,0};
+    radius = _radius;
+}
+
+void Circle::setCenter(Point _center)
+{
+    center = _center;
+}
+void Circle::serRadius(double _radius)
+{
+    radius = _radius;
+}
+
+Point Circle::getCenter() const
+{
+    return center;
+}
+double Circle::getRadius() const
+{
+    return radius;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "Graph.h"
+#include "Geometry.h"
 
 void Test(unsigned iter)
 {
